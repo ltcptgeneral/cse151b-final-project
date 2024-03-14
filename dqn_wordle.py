848ea719b75f39429f3db37c4dd4f1781e69e034
Arthur Lu <alu@tronnet.net>
2024-03-13 21:36:26 -0700

    still doesnt train

--- dqn_wordle.py
+++ dqn_wordle.py
@@ -1,47 +1,40 @@
-# %%
-from stable_baselines3 import DQN
-import numpy as np
-import wordle.state
 import gym
+import sys
+from stable_baselines3 import DQN
+from stable_baselines3.common.env_util import make_vec_env
+import wordle_gym
+import numpy as np
+from tqdm import tqdm
 
-# %%
-env = gym.make("WordleEnvFull-v0")
+def train (model, env, total_timesteps = 100000): 
+    model.learn(total_timesteps=total_timesteps, progress_bar=True)
+    model.save("dqn_wordle")
 
-print(env)
+def test(model, env, test_num=1000):
 
-# %%
-total_timesteps = 100000
-model = DQN("MlpPolicy", env, verbose=0)
-model.learn(total_timesteps=total_timesteps, progress_bar=True)
+    total_correct = 0
 
-# %%
-def test(model):
+    for i in tqdm(range(test_num)):
 
-    end_rewards = []
+        model = DQN.load("dqn_wordle")
 
-    for i in range(1000):
-        
-        state = env.reset()
-
+        env = gym.make("wordle-v0")
+        obs = env.reset()
         done = False
-
         while not done:
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
 
-            action, _states = model.predict(state, deterministic=True)
+            print(action, obs, rewards)
 
-            state, reward, done, info = env.step(action)
-            
-        end_rewards.append(reward == 0)
-        
-    return np.sum(end_rewards) / len(end_rewards)
+    return total_correct / test_num
 
-# %%
-model.save("dqn_wordle")
+if __name__ == "__main__":
+    
+    env = gym.make("wordle-v0")
+    model = DQN("MlpPolicy", env, verbose=0)
+    print(env)
+    print(model)
 
-# %%
-model = DQN.load("dqn_wordle")
-
-# %%
-print(test(model))
-
-
+    train(model, env, total_timesteps=10000)
+    print(test(model, env, test_num=1))
